f8204c3ec6bc38851c3b28847ab2597ec90da3d7
Simon Henderson <simonhenderson100@gmail.com>
2023-09-07 13:36:13 -0700

    Save Push
    Been working a lot on the Enemy AI, might as well save

---
+++ Sylvan Larceny/Assets/Scripts/Enemy Scripts/EnemyDetectPlayer.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class EnemyDetectPlayer : MonoBehaviour
+{
+    PolygonCollider2D detectCol;
+
+    EnemyMove eMoveScript;
+
+    RaycastHit2D hit;
+
+    void Start()
+    {
+        detectCol = GetComponent<PolygonCollider2D>();
+
+        eMoveScript = GetComponentInParent<EnemyMove>();
+
+        if (detectCol == null)
+        {
+            Debug.LogError("EnemyDetectPlayer cannot access a PolygonCollider2D");
+        }
+
+        if (detectCol == null)
+        {
+            Debug.LogError("EnemyDetectPlayer cannot access an EnemyMoveScript");
+        }
+    }
+
+    // Logic for when the Enemy is in the PATROLING state (or the CHASINGLASTSEEN ig)
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && eMoveScript.state != EnemyState.CHASINGPLAYER)
+        {
+            // Check to make sure that the player didn't hit the collider through a wall or something
+            hit = Physics2D.Raycast(transform.position, eMoveScript.player.transform.position, Mathf.Infinity, ~eMoveScript.playerAndEnemy);
+            
+            if (hit.collider == null)
+            {
+                eMoveScript.state = EnemyState.CHASINGPLAYER;
+            }
+        }
+    }
+
+    // Logic for when the Enemy is in the other two states (CHASINGPLAYER and CHASINGLASTSEEN)
+    void Update()
+    {
+        if (eMoveScript.state == EnemyState.CHASINGPLAYER)
+        {
+
+        }
+        else if (eMoveScript.state == EnemyState.CHASINGLASTSEEN)
+        {
+
+        }
+    }
+
+}
--- Sylvan Larceny/Assets/Scripts/Enemy Scripts/EnemyMove.cs
+++ Sylvan Larceny/Assets/Scripts/Enemy Scripts/EnemyMove.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+
+public enum EnemyState { PATROLING, CHASINGPLAYER, CHASINGLASTSEEN}
 
 public class EnemyMove : MonoBehaviour
 {
     Transform tf;
     Rigidbody2D rb;
 
-    PolygonCollider2D detectCol;
+    EnemyDetectPlayer detectScript;
+
+    public GameObject player;
+    AdrenalineMode AMScript;
+
+    public EnemyState state;
+
+    public LayerMask playerAndEnemy;
 
     // 1 = Stationary, 2 = Rotating, 3 = Patroling, 4 = Wandering
     public int enemyType;
 
-    bool chasingPlayer = false;
+    bool moving;
 
     // 1 = N, 2 = E, 3 = S, 4 = W
     int faceDir;
+
+    RaycastHit2D hit;
 
     void Start()
     {
         tf = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
-        detectCol = GetComponentInChildren<PolygonCollider2D>();
+
+        detectScript = GetComponentInChildren<EnemyDetectPlayer>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        AMScript = player.GetComponent<AdrenalineMode>();
 
         if (tf == null)
         {
@@ -33,21 +49,67 @@ public class EnemyMove : MonoBehaviour
             Debug.LogError("EnemyMovement on " + this.name + "script cannot access a Rigidbody2D");
         }
 
-        if (detectCol == null)
+        if (detectScript == null)
         {
-            Debug.LogError("EnemyMovement on " + this.name + "script cannot access a PolygonCollider2D");
+            Debug.LogError("EnemyMovement on " + this.name + "script cannot access an EnemyDetectPlayer script");
+        }
+
+        if (player == null)
+        {
+            Debug.LogError("EnemyMovement on " + this.name + "script cannot access the player");
+        }
+
+        if (AMScript == null)
+        {
+            Debug.LogError("EnemyMovement on " + this.name + "script cannot access the AMScript");
+        }
+
+        state = EnemyState.PATROLING;
+
+        moving = false;
+
+        playerAndEnemy = LayerMask.GetMask("Player");
+        playerAndEnemy = LayerMask.GetMask("Enemy");
+    }
+
+    void Update()
+    {
+        if (state == EnemyState.PATROLING)
+        {
+
+        }
+        else if (state == EnemyState.CHASINGPLAYER)
+        {
+            hit = Physics2D.Raycast(transform.position, player.transform.position, Mathf.Infinity, ~playerAndEnemy);
+            while (hit.collider == null)
+            {
+                if (!moving)
+                {
+                    StartCoroutine(EnemyChaseMove(player.transform.position.x, player.transform.position.y));
+                }
+            }
+        }
+        else if (state == EnemyState.CHASINGLASTSEEN)
+        {
+
         }
     }
 
-    public void EnemyTurn()
+    IEnumerator EnemyPatrolMove()
     {
-        if (chasingPlayer)
-        {
-            // Target the player and chase them down
-        }
-        else
-        {
-            // Continue on a patrol route
-        }
+        moving = true;
+
+        yield return null;
+
+        moving = false;
+    }
+
+    IEnumerator EnemyChaseMove(float targetX, float targetY)
+    {
+        moving = true;
+
+        yield return null;
+
+        moving = false;
     }
 }
