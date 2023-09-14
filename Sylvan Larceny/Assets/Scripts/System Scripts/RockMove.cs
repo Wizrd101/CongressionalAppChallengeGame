ed13fa2d6e5aa551fc9e4c234e915f054aeec576
Simon Henderson <simonhenderson100@gmail.com>
2023-09-14 13:56:14 -0700

    End of Day 9/14
    Solution to admin rights by Friday
    
    (In case IT forgets, the internet never will)

--- Sylvan Larceny/Assets/Scripts/System Scripts/RockMove.cs
+++ Sylvan Larceny/Assets/Scripts/System Scripts/RockMove.cs
@@ -10,47 +10,107 @@ using UnityEngine;
 
 public class RockMove : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    // Rock Components
+    Rigidbody2D rb;
+    BoxCollider2D bc;
 
+    // The Player's Rock-Throwing script
     ThrowRock tr;
 
-    [SerializeField] float powerCoefficient;
-    [SerializeField] float wallFrictionCoefficient;
+    // Different Coefficients that are needed for calculations
+    float startPowerCoefficient;
+    float dropOffCoefficient;
+    float wallFrictionCoefficient;
 
+    // Different checks to determine when the rock stops moving
     bool rockMoving;
+    bool xMoveCheck;
+    bool yMoveCheck;
 
+    // Rock velocity info
     int xDir;
     int yDir;
     float rockVelo;
     Vector2 tempVelo;
 
-    void Update()
+    // FixedUpdate is for checking if the rock is still moving, and decreasing it's velocity if it is (so it can eventually stop moving)
+    void FixedUpdate()
     {
-        
+        if (rockMoving)
+        {
+            if (rb.velocityX > 0)
+            {
+                rb.velocityX -= dropOffCoefficient;
+                Mathf.Clamp(rb.velocityX, 0, Mathf.Infinity);
+            }
+            else if (rb.velocityX < 0)
+            {
+                rb.velocityX += dropOffCoefficient;
+                Mathf.Clamp(rb.velocityX, Mathf.NegativeInfinity, 0);
+            }
+            else
+            {
+                xMoveCheck = true;
+            }
+
+            if (rb.velocityY > 0)
+            {
+                rb.velocityY -= dropOffCoefficient;
+                Mathf.Clamp(rb.velocityY, 0, Mathf.Infinity);
+            }
+            else if (rb.velocityY < 0)
+            {
+                rb.velocityY += dropOffCoefficient;
+                Mathf.Clamp(rb.velocityY, Mathf.NegativeInfinity, 0);
+            }
+            else
+            {
+                yMoveCheck = true;
+            }
+            
+            if (xMoveCheck && yMoveCheck)
+            {
+                RockStopMoving();
+            }
+        }
     }
 
+    // Void called by ThrowRock. Declares a bunch of variables that I can't declare in Start (as I found out lol)
     public void SetUpProjectile()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
 
-        if (powerCoefficient == 0)
-        {
-            powerCoefficient = 1;
-        }
+        startPowerCoefficient = 3;
+        dropOffCoefficient = 0.1f;
+        wallFrictionCoefficient = 2;
 
+        rockMoving = true;
+        xMoveCheck = false;
+        yMoveCheck = false;
+        
         tempVelo = Vector2.zero;
     }
 
+    // Sets the movement direction and power that the rock will be thrown
     public void SetDir(int xMove, int yMove, float power)
     {
         xDir = xMove;
         yDir = yMove;
-        rockVelo = power * powerCoefficient;
+        rockVelo = power * startPowerCoefficient;
     }
 
+    // Actually initializes the movement of the rock
     public void ActivateProjectile()
     {
         rb.velocity = new Vector2(xDir * rockVelo, yDir * rockVelo);
+    }
+
+    // Void that is called when the rock stops moving (to turn it's collider into a trigger,
+    void RockStopMoving()
+    {
+        rockMoving = false;
+        bc.isTrigger = true;
     }
 
     void OnCollisionEnter2D(Collision2D other)
