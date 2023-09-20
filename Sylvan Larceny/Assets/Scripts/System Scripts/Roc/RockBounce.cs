7f97926fe77a7b839a796bc518d677c3a43ef327
Simon Henderson <simonhenderson100@gmail.com>
2023-09-20 12:52:42 -0700

    Rock Bouncing Fixed
    Yes it took that long, I had to overhaul my old solution

---
+++ Sylvan Larceny/Assets/Scripts/System Scripts/Roc/RockBounce.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBounce : MonoBehaviour
+{
+    public Rigidbody2D rockRb;
+
+    public RockMove rockMoveScript;
+
+    void Update()
+    {
+        if (!rockMoveScript.rockMoving)
+        {
+            this.enabled = false;
+        }
+    }
+
+    public void SetUpBounceScript()
+    {
+        rockRb = this.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>();
+
+        rockMoveScript = this.gameObject.GetComponentInParent<RockMove>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (this.gameObject.name == "LeftCol" || this.gameObject.name == "RightCol")
+        {
+            rockRb.velocityX = -rockRb.velocityX;
+        }
+        else if (this.gameObject.name == "UpCol" || this.gameObject.name == "DownCol")
+        {
+            rockRb.velocityY = -rockRb.velocityY;
+        }
+        else
+        {
+            Debug.LogWarning("Extra RockBounce called");
+        }
+
+        rockMoveScript.DecreaseVelocity(rockMoveScript.wallFrictionCoefficient / rockRb.velocity.magnitude);
+    }
+}
