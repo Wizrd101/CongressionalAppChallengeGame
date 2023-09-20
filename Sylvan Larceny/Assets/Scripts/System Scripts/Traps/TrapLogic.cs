448e359e0637ebf4ba470b342bd126c2bb1bf020
Simon Henderson <simonhenderson100@gmail.com>
2023-09-20 13:56:44 -0700

    Started working on traps

---
+++ Sylvan Larceny/Assets/Scripts/System Scripts/Traps/TrapLogic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField] enum TrapType { NOTSET, ARROW, NOISE }
+
+public class TrapLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
