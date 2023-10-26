ec54f888d8538030d3342be4a733b8a1589b6e35
Simon Henderson <simonhenderson100@gmail.com>
2023-10-26 11:43:20 -0700

    Finished up with the Gem pickup
    In the levels at least, the gem is finished
    
    I need to work on the Gem in the main menu, and I also rediscovered that I never finished the caves, so that's also on my hit list today.

--- Sylvan Larceny/Assets/Scripts/Menu Scripts/InteractPromptCanvasEnable.cs
+++ Sylvan Larceny/Assets/Scripts/Menu Scripts/InteractPromptCanvasEnable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InteractPromptCanvasEnable : MonoBehaviour
@@ -8,11 +9,19 @@ public class InteractPromptCanvasEnable : MonoBehaviour
 
     InteractPointController ipc;
 
+    TextMeshProUGUI cvTxt;
+    [SerializeField] string customText;
+
     void Start()
     {
         thisCv = GetComponent<Canvas>();
 
         ipc = GameObject.FindWithTag("Player").GetComponentInChildren<InteractPointController>();
+
+        cvTxt = GetComponentInChildren<TextMeshProUGUI>();
+
+        if (customText.Length != 0)
+            cvTxt.text = customText;
     }
 
     // Update is called once per frame
